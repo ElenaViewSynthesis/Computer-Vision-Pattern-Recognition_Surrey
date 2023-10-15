ef22e6aa4f4b1beb7cfdea91656ba19ec8ddd3bc
AI/ML Engineer <145933797+MylonaH@users.noreply.github.com>
2023-10-15 02:03:04 +0100

    Updated quantization level for better visualization on histogram

--- Visual Search of Image Collection/code/GCH_desc.m
+++ Visual Search of Image Collection/code/GCH_desc.m
@@ -8,7 +8,7 @@ OUT_SUBFOLDER ='globalRGBhisto';
 
 
 allfiles=dir (fullfile([DATASET_FOLDER,'/Images/*.bmp']));
-QuantzLevel = 8;
+QuantzLevel = 8; %3,4,5,6,7
 for filenum=1:length(allfiles)
     fname=allfiles(filenum).name;
     fprintf('Processing file %d/%d - %s\n',filenum,length(allfiles),fname);
@@ -16,7 +16,7 @@ for filenum=1:length(allfiles)
     imgfname_full=([DATASET_FOLDER,'/Images/',fname]);
 
     % Image where RGB are NORMALISED in range 0 to (quantiz_level-1) by dividing each pixel value by 256
-    img = double(imread(imgfname_full)); %% remove
+    img = double(imread(imgfname_full)); %./255%% remove
     fout=[OUT_FOLDER,'/',OUT_SUBFOLDER,'/',fname(1:end-4),'.mat'];
    
     F=GCH_generator(img,QuantzLevel);
