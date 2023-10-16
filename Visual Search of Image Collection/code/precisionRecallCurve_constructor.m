5041cb30577e1bba7d1cfcd257095e81f780685b
AI/ML Engineer <145933797+MylonaH@users.noreply.github.com>
2023-10-16 01:56:24 +0100

    Construction of a curve to be filled in with features matrix

---
+++ Visual Search of Image Collection/code/precisionRecallCurve_constructor.m
@@ -0,0 +1,11 @@
+function [outputArg1] = precisionRecallCurve_constructor(distance)
+% pass queryImgIndex, allFeat, allClass, classIndex, classCounts, technique
+
+
+
+
+
+% https://www.youtube.com/watch?v=k-qgz1N5l7Y
+
+
+end
