c4518c8c4ef30ef753f6a6543159398f7d77bbb2
AI/ML Engineer <145933797+MylonaH@users.noreply.github.com>
2023-10-17 03:21:59 +0100

    Need to compute the precision & recall values in the for loop

--- Visual Search of Image Collection/code/precisionRecallCurve_constructor.m
+++ Visual Search of Image Collection/code/precisionRecallCurve_constructor.m
@@ -3,33 +3,64 @@ function [precision, recall] = precisionRecallCurve_constructor(numImgs, distSim
 % Get the name of the image to query within the class it belongs to.
 classImgIndex=ImgsCollection(classImgQuery).name;
 
-groundTruthVector = zeros([1, numImgs-1]); % Assuming all images are initially irrelevant
+groundTruthVector = zeros([1, numImgs-1]);
 
-% Convert img index name to character array
+% Convert img index name to character array-matrix in order to access individual
+% characters using indexing.
+classImgIndexChar = char(classImgIndex);
+% Extract the first two chars from the char matrix to determine the class of query image
+retrievedImgClass = classImgIndexChar(1:2);
+% (1:2) captures the category information, which is typically encoded in the first two characters of the file name.
+disp(retrievedImgClass); % TO REMOVE
 
+numEachImgCategory = 30;
+totalElements = 0; %% Last Image class_20 does not contain 30 bmp elements like the other classes
+
+% Count total image elements within the interest image class
+for b=1:length(ImgsCollection)
+    classes = char(ImgsCollection(b).name);
+    if classes(1:2) == retrievedImgClass
+        totalElements = totalElements +1; % += operator not recognised in MATLAB
+    end
+end
 
 % Top 5 are relevant images in the ground truth
-relevantImages = distSimilarities(1:5, 2); % Need to adjust based on the ACTUAL RELEVANCE criteria
-groundTruthVector(relevantImages)=1;
+%relevantImages = distSimilarities(1:5, 2);
+%groundTruthVector(relevantImages)=1;
 
 % Initialise arrays to store precision and recall values
-precision = zeros([1, numImgs-1]); % 1D array initialised with zeros
+truePositives = 0;
+precision = zeros([1, numImgs-1]); % 1D array
 recall = zeros([1, numImgs-1]); 
 
 % Compute precision and recall at each threshold
-for i = 1:numImgs
-    retrievedImages = distSimilarities(1:i, 2); % Gives error IndexOutOfBounds -> DELETE
-
+%for i = 1:numImgs
+ %   retrievedImages = distSimilarities(1:i, 2); 
     % Calculate the true positives (Relevant Images Retrieved)
-    truePositives = sum(groundTruthVector(retrievedImages));
+  %  truePositives = sum(groundTruthVector(retrievedImages));
     % Precision  at i retrieved images
-    precision(i) = truePositives / i;
+   % precision(i) = truePositives / i;
     % Recall at i retrieved images
-    recall(i) = truePositives / sum(groundTruthVector);
+    %recall(i) = truePositives / sum(groundTruthVector);
+%end
+
+for k=1:size(distSimilarities,1)
+    imgRankedName = char(ImgsCollection(distSimilarities(k,2)).name);
+    imgRankedClass = imgRankedName(1:2);
+    if imgRankedClass == retrievedImgClass
+        truePositives = truePositives + 1;
+    end
+    
 end
 
-numEachImgCategory = 30;
+
 % queryFeature
+
+
+
+
+% If not working => Histogram of each Image Class (histoImgClass)
+
 
 
 % Plot the PR Curve
