ab4c54b8f599d8ee638b9fb5b5289a9ee9501e30
AI/ML Engineer <145933797+randomHersey@users.noreply.github.com>
2023-10-18 18:40:57 +0100

    PR constructor running some issues with non numerical value for division

---
+++ Visual Search of Image Collection/precisionRecallCurve_constructor.m
@@ -0,0 +1,121 @@
+function precisionRecallCurve_constructor(numImgs, distSimilarities, classImgQuery, ImgsCollection, ImgCategories)
+
+% Get the name of the image to query within the class it belongs to.
+classImgIndex=ImgsCollection(classImgQuery).name;
+
+groundTruthVector = zeros([1, numImgs-1]);
+
+
+% Convert img index name to character array-matrix in order to access individual
+% characters using indexing.
+classImgIndexChar = char(classImgIndex);
+% Extract the first two chars from the char matrix to determine the class of query image
+retrievedImgClass = classImgIndexChar(1:2);
+% (1:2) captures the category information, which is typically encoded in the first two characters of the file name.
+%disp(retrievedImgClass); % TO REMOVE
+
+% OBSERVE the Images foler in the dataset => approx 30 images for each category
+numEachImgCategory = 30;
+numCategories = 20;
+totalElements = 0; %% Last Image class_20 does not contain 30 bmp elements like the other classes
+
+relevantCategories = histogram(ImgCategories,numCategories); % histogram with 20 bins
+quantityOfCateg = size(relevantCategories,2);
+CONFUSION = zeros(quantityOfCateg);
+categoryPos = [301;352;382;412;442;472;502;532;562;1;33;63;97;127;157;181;211;241;271;331];
+%categoryPos = [1;33;63;97;127;157;181;211;241;271;301;331;352;382;412;442;472;502;532;562];
+
+
+% Count total image elements within the interest image class
+%for b=1:length(ImgsCollection)
+%    classes = char(ImgsCollection(b).name);
+%    if classes(1:2) == retrievedImgClass
+%        totalElements = totalElements +1; % += operator not recognised in MATLAB
+%    end
+%end
+
+% Top 5 are relevant images in the ground truth
+%relevantImages = distSimilarities(1:5, 2);
+%groundTruthVector(relevantImages)=1;
+
+% Initialise arrays to store precision and recall values
+
+precision = zeros([1, numImgs-1]); % 1D array
+recall = zeros([1, numImgs-1]); 
+
+AVGPRECISION = [];
+for labelPos=1:quantityOfCateg
+
+% Compute precision and recall at each threshold
+%for i = 1:numImgs
+ %   retrievedImages = distSimilarities(1:i, 2); 
+    % Calculate the true positives (Relevant Images Retrieved)
+  %  truePositives = sum(groundTruthVector(retrievedImages));
+    % Precision  at i retrieved images
+   % precision(i) = truePositives / i;
+    % Recall at i retrieved images
+    %recall(i) = truePositives / sum(groundTruthVector);
+%end
+    target = categoryPos(labelPos);
+    
+truePositives = 0;
+auxPvalue = 0;
+
+p = [];
+r = []; 
+
+
+% If not working => Histogram of each Image Class
+
+% CHECK for two digits fileame with extra IF
+%for k=1:size(distSimilarities,1)
+%    imgRankedName = char(ImgsCollection(distSimilarities(k,2)).name);
+%    imgRankedClass = imgRankedName(1:2);
+%    if imgRankedClass == retrievedImgClass
+%        truePositives = truePositives + 1; %match_count
+%    end
+    for k=1:size(distSimilarities,1)
+        imgRankedCategory = ImgCategories(distSimilarities(k,2),:);
+        if imgRankedCategory == labelPos
+            truePositives = truePositives +1;
+        end
+
+
+        precisionValues = truePositives / k;
+        recallValues = truePositives / relevantCategories(labelPos);
+        % Append the new value to the recall array r as a new row.
+        r = [r ; recallValues]; 
+        p = [p ; precisionValues];
+        %if
+         %   fff
+        end
+    end
+
+
+% queryFeature
+
+
+
+% [X,Y] = perfcurve(labels,scores,posclass);
+
+% Plot the PR Curve
+plot(r, p, 'LineWidth', 2, 'Marker','o');
+xlabel('Recall');
+ylabel('Precision');
+title('Precision Recall Curve');
+xlim([0 1]); % Limit values to range from 0 to 1
+ylim([0 1]);
+%legend('house', 'car', 'tree')
+figure;
+
+
+
+% https://www.youtube.com/watch?v=k-qgz1N5l7Y
+
+% Compute MAP over all queries
+% For Average Precision 
+%avgP = p .* 
+
+% ap = trapz(r,p);
+
+return;
