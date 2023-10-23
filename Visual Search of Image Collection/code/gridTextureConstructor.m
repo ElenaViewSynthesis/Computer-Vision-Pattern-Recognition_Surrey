69ba74b7bb46c6592d07b78cded968b807a9c2ba
AI/ML Engineer <145933797+randomHersey@users.noreply.github.com>
2023-10-23 02:08:54 +0100

    Calculate differentiation values in respect to x and y separately

--- Visual Search of Image Collection/code/gridTextureConstructor.m
+++ Visual Search of Image Collection/code/gridTextureConstructor.m
@@ -13,6 +13,7 @@ E5 = [-1 0 1];
 S5 = [1 2 1];
 R5 = [1 -2 1];
 
+blur = [];
 textureFeatures = [];
 
 firstElementOfSizeArray = 1;
@@ -21,6 +22,9 @@ secondElementOfSizeArray = 2;
 targetImgSize = size(targetImg);
 targetImgR = targetImgSize(firstElementOfSizeArray);
 targetImgC = targetImgSize(secondElementOfSizeArray);
+
+xSobel = [S5 ; 0 0 0; -1 -2 -1];
+ySobel = xSobel';
 
 for r=1:griDivision
     for c=1:griDivision
@@ -32,14 +36,43 @@ for r=1:griDivision
         targetImgCell = targetImg(startingRowPosition:endingRowPosition, startingColPosition:endingColPosition, :);
         
         % The image must be grayscaled
-        grayImgCell = rgb2gray(targetImgCell);
+        grayImgCell = double(rgb2gray(targetImgCell));
+           
+        % the structure of the image can be given by edges
+        % Edge Detecting filter -> Sobel
+        % Run a convolution over the image using differentiation df/dx and df/dy
+        xDiffSobel = 0;
+        yDiffSobel = 0;
+
+
+        % Get the edgeMagnitude (square them up individually, addition, sqrt of sum)
         
 
+        %% Now, compute the orientation of edges
+        %% Estimation of edge orientation theta with atan((df/dy) / (df/dx))
+        % theta = 0-360 || 0-2pi
+        
+
+        % Quantise orientation into 8 bins (0-2pi)
+        
+
+        % EOH (frequency) histogram depicts the frequency of pixels that for every value of theta of
+        % pixel inside the cell belongs to one of the bins 
+        
+        
+        % Avoid noisy regions by checking if theta is greater than threshold
+        %% Normalise prior to concatenation 
+            
+         %E = [E ComputeEdgeOrientationHistogram(subImage)];
+        %C = ComputeGlobalColour(subImage);
+         % C(1) = Average Red, C(2) = Average Green, C(3) = Average Blue
+        %A = [A C(1) C(2) C(3)];
+        
 
     end
 end
 
-
+%G = [E A];
 
 
 
