c9e336622d789cd87d96239a5f9ddd9da7e77cbf
AI/ML Engineer <145933797+randomHersey@users.noreply.github.com>
2023-10-25 22:10:26 +0100

    Compute avg colour channels of the image cell using function

--- Visual Search of Image Collection/code/mergedCEOH.m
+++ Visual Search of Image Collection/code/mergedCEOH.m
@@ -5,14 +5,39 @@ function CEOHdescriptor = mergedCEOH(targetImage, cell, quantizationLevels, cutO
 xSobel = [1 2 1; 0 0 0; -1 -2 -1];
 ySobel = xSobel';
 
-% Normalise Sobel filters for both dimensions
-
-% Determine the rows and columns
-targetImgSize = size(targetImage);
-
+% Determine the rows and columns.     
 CEOHdescriptor = [];
+grayScaledImage = rgb2gray(targetImage);
+[grayImgRow, grayImgCol, ~] = size(grayScaledImage);
+
+blur = [];
+textureFeatures = [];
+
+% Normalise Sobel filters for both dimensions.
 
 
+for r=1:cell
+    for c=1:cell
+        startingRowPosition = round((r-1)*grayImgRow/cell); % to not be multiplied by zero
+        if startingRowPosition == 0
+            startingRowPosition = 1;
+        end
+         endingRowPosition = round(r*grayImgRow/cell);
+
+
+        startingColPosition = round((c-1)*grayImgCol / cell); % (cell+1) without if ?
+        if startingColPosition  == 0
+            startingColPosition = 1;
+        end
+        endingColPosition = round((c*grayImgCol/cell));
+        
+        grayScaledImgCell = grayScaledImage(startingRowPosition:endingRowPosition, startingColPosition:endingColPosition, :);
+        % Now compute mean color channels of current gray image cell
+        
+
+
+    end
+end
 
 
 
