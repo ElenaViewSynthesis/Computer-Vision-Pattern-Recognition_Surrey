4936281896517cfc611f79b01d45b4f95e57da81
AI/ML Engineer <145933797+randomHersey@users.noreply.github.com>
2023-10-26 23:56:48 +0100

    Minkowski distance metric for comparison to be implemented

---
+++ Visual Search of Image Collection/code/compareMinkowskiDist.m
@@ -0,0 +1,41 @@
+function MinkoDis = compareMinkowskiDist()
+% A generalization of both Euclidean and Manhattan distances, the Minkowski distance 
+% is controlled by a parameter 'p.' When p=2, it's equivalent to the Euclidean distance, 
+% and when p=1, it's equivalent to the Manhattan distance.
+
+
+
+                    %% check GPT
+               % \left(\sum_{i=1}^n |x_i-y_i|^p\right)^{1/p}
+                  
+
+
+end
+
+                                            %% Common Distance Metrics
+% Cosine Similarity: This metric measures the cosine of the angle between two non-zero vectors. 
+% It's often used in text analysis and image retrieval when you want to measure the orientation rather than the magnitude of vectors.
+
+% Hamming Distance: Primarily used for binary data like binary images or strings, it counts 
+% the number of positions at which the corresponding elements of two vectors are different.
+
+% Jaccard Similarity: Commonly used for sets or binary data, it measures the size of the intersection 
+% of sets divided by the size of the union of sets. It's useful in applications like image retrieval and document analysis.
+
+% Mahalanobis Distance: This metric takes into account the covariance structure of the data and is useful 
+% when the data is not isotropic or when different features have different scales. It's often used in 
+% object classification and clustering.
+
+% Chebyshev Distance (L∞ Norm): It's the maximum absolute difference between any coordinate of two points. 
+% This is a suitable metric when you want to measure dissimilarity based on the most significant difference between points.
+
+% Correlation Distance: Measures the correlation between two vectors. It's used for finding similarities 
+% between patterns when the absolute values are less important than the correlation structure.
+
+% Dynamic Time Warping (DTW): A distance metric used for comparing time series data. It allows for the alignment of similar sequences with variations in time and amplitude.
+
+% Bhattacharyya Distance: Commonly used in image processing and computer vision, it quantifies the statistical 
+% similarity between two probability distributions, making it suitable for comparing histograms or pixel values in images.
+
+% Earth Mover's Distance (EMD): Often used in image retrieval and object matching, it calculates the minimum 
+% cost required to transform one distribution into another. It's particularly useful for comparing histograms.
