172b19a2f9c992a94132a51d53deefdd8c50eb6a
AI/ML Engineer <145933797+randomHersey@users.noreply.github.com>
2023-10-30 01:11:14 +0000

    Computed principal components of data space
    Calculated the eigenvalues & eigenvectors of the covariance matrix

--- Visual Search of Image Collection/code/buildEigenModelFromObservations.m
+++ Visual Search of Image Collection/code/buildEigenModelFromObservations.m
@@ -8,11 +8,42 @@
 %%                        - E.N   - number of observations used, n.
 %%                        
 %% LICENSE: John Collomosse 2010  (J.Collomosse@surrey.ac.uk)
-function EM = buildEigenModelFromObservations(observationsFEAT)
 
-    Eigen.Num = size(observationsFEAT, 2);
-    Eigen.Drow = size(observationsFEAT, 1);
- 
+
+
+
+function EigenM = buildEigenModelFromObservations(observationsFEAT)
+% The observations are used to compute the covariance matrix (C) and subsequently find the eigenvectors 
+% and eigenvalues of this covariance matrix. The eigenvectors capture the principal components or 
+% directions of maximum variance in the data, and the eigenvalues represent the amount 
+% of variance explained by each corresponding eigenvector.
+
+    EigenM.Num = size(observationsFEAT, 2); % the dot '.' is used for element-wise operations on structures, is applied to each element of the structure.
+    EigenM.Drow = size(observationsFEAT, 1);
+    % computes the mean along each row of the FEAT matrix -> represents the mean for each dimension.
+    EigenM.MeanPerDimension = mean(observationsFEAT')';
+
+    % subtracting the mean values stored in the  EigenM.MeanPerDimension from each observation in 
+    % observationsFEAT => aka centering the data because it shifts the data points such that 
+    % the mean of each dimension in the centered data (stored in centered_FEAT_observations) becomes zero.
+    % Briefly, it contains the centered/mean-subtracted observations.
+    centered_FEAT_observations = observationsFEAT - repmat(EigenM.MeanPerDimension,1,EigenM.Num); 
+    
+    % Matrix to capture the covariance relationships between the different dimensions of the data & 
+    % Identify the directions of maximum variance.
+    % It quantifies how the different dimensions of your data arecorrelated with each other.
+    % finding the Principal Components (eigenvectors) and their corresponding variances (eigenvalues). 
+    DIMENSIONAL_COVARIANCE_MATRIX = (1/EigenM.Num) * (centered_FEAT_observations * centered_FEAT_observations');
+    
+    % Each column of U represents an eigenvector.
+    % The eigenvalues are stored as the diagonal elements of a matrix, and V represents that matrix.
+    % The eigenvectors represent the principal components, which are the directions of maximum 
+    % variance in the data. The corresponding eigenvalues indicate 
+    % the amount of variance explained by each principal component.
+    [U V] = eig(DIMENSIONAL_COVARIANCE_MATRIX);
+    
+    % here
+    
 
 % ReduceDimensionalityWithPCA
 
