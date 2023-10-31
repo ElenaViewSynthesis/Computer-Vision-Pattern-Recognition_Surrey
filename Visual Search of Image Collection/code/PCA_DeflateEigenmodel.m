f99534299ba3c08620b2b6b298bb91ed031e29ff
AI/ML Engineer <145933797+randomHersey@users.noreply.github.com>
2023-10-31 13:40:50 +0000

    Reduced the complexity of Eigen model while retaining the most significant eigenvectors by the deflation method.

--- Visual Search of Image Collection/code/PCA_DeflateEigenmodel.m
+++ Visual Search of Image Collection/code/PCA_DeflateEigenmodel.m
@@ -12,6 +12,7 @@
 %%                     
 %% (c) John Collomosse 2010  (J.Collomosse@surrey.ac.uk)
 function EigenM = PCA_DeflateEigenmodel(E, method, param)
+    % The 'param' variable determines the desired percentage of energy.
     switch method
         case 'keepn'                                   
             EigenM.val = EigenM.val(1:param); % Select only the first 'param' eigenvalues, the MOST significant ones.
@@ -20,11 +21,32 @@ function EigenM = PCA_DeflateEigenmodel(E, method, param)
             totalenergy = sum(abs(EigenM.val)); % total energy represents the total variance in the data.
             currentenergy = 0;                  % track how much energy has been accumulated.
             rank = 0;                           % counter of eigenvalues that reach the specified energy threshold.
-        
-            % for loop here
-
+            
+            % Process each eigenvalue One by One and their associated eigenvectors => 
+            % Iterate through the eigenvalues in descending order (from the most significant to the least significant).
+            for eigenValueIndex = 1:size(EigenM.vct,2)      % Iterate through the number of eigenvectors columns
+                if currentenergy > (totalenergy * param)    % Check whether the current accumulated energy exceeds a specified precentage of the total energy.
+                    break;                      % If the accumulated energy surpasses the desired threshold, then exit
+                end                             % since it reached the desired level of retained energy.
+                rank = rank + 1;                % tracker of eigenvalues which have not exceeded the threshold.
+                currentenergy = currentenergy + EigenM.val(eigenValueIndex);
+                % Current eigenvalue is added to the accumulated energy at each iteration (cumulative energy).
+                % Incrementally calculate the energy explained by each eigenvalue, starting with the most significant.
+            end
+            EigenM = PCA_DeflateEigenmodel(EigenM, 'keepn', rank);
+            % Keep only the top 'rank' most significant eigenvectors and their associated 
+            % eigenvalues. This reduces the dimensionality of the eigenmodel, as specified by the 'keepn' deflation method.
     end
-
+    % The 'rank' var stores the number of eigenvalues needed to retain the specified percentage of total variance in the data. 
+    %% Total energy indicates how much of the total variance in the data is explained by each principal component. 
+    %% By monitoring this cumulative energy=> make decisions about how many principal components to retain to capture a certain percentage of the total variance.
+    
+    %% Larger eigenvalues indicate that the associated eigenvectors capture more 
+    %% of the data's variability, while smaller eigenvalues correspond to less significant dimensions or components.
+    
+    %% Energy or Variance in the Data: In the context of PCA, "energy" or "variance" refers 
+    %% to the amount of variability in the dataset. It quantifies how much the data points 
+    %% deviate from the mean or central tendency. More variance indicates more spread or dispersion in the data.
 
 
 
