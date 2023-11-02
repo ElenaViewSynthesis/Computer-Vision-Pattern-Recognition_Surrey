8fab81abef95a26ed30f01f9cca72542a42e44f0
AI/ML Engineer <145933797+randomHersey@users.noreply.github.com>
2023-11-02 12:28:40 +0000

    Centered Feature points at origin & the directions of *Maximum Variance* around are accessible now

--- Visual Search of Image Collection/code/PCA_ProjectDataOntoEigenModel.m
+++ Visual Search of Image Collection/code/PCA_ProjectDataOntoEigenModel.m
@@ -8,9 +8,15 @@
 %% (c) John Collomosse 2010
 function  projectedObs = PCA_ProjectDataOntoEigenModel(observationsFEAT, EigenM)
     
+    % Centers the data/observation points by subtracting the mean of the data - FeaturesCenteredAtOrigin. 
     meanCenteredFEATobservations = observationsFEAT - repmat(EigenM.MeanPerDimension, 1, size(observationsFEAT,2));
-    projectedObs = EigenM.vct' * meanCenteredFEATobservations;
-    
+    projectedObs = EigenM.vct' * meanCenteredFEATobservations; % perform the actual projection of the translated/centered data 
+                                                               % onto the eigenmodel basis by multiplying the transpose of the 
+                                                               % eigenvectors matrix (EigenM.vct) with the centred feature points.
+                                                               % It projects the data points onto the principal components represented 
+                                                               % by the eigenvectors, effectively reducing the dimensionality of the data.
+
+%% Each column of 'projectedObs' corresponds to a projected feature/data point in this reuced-dimensional space. 
 % The translated data will be used for PCA & the principal components (eigenvectors) obtained from this
 % data represent the directions of *Maximum Variance* around the origin.
 
