ff291b6be1fb274bf42c41886f80f8a095d44f2a
AI/ML Engineer <145933797+randomHersey@users.noreply.github.com>
2023-11-11 19:58:53 +0000

    Updated all descriptors files in main program

--- Visual Search of Image Collection/code/cvpr_computedescriptors.m
+++ Visual Search of Image Collection/code/cvpr_computedescriptors.m
@@ -1,51 +1,62 @@
-%% EEE3032 - Computer Vision and Pattern Recognition (ee3.cvpr)
-%%
-%% cvpr_computedescriptors.m
-%% Skeleton code provided as part of the coursework assessment
-%% This code will iterate through every image in the MSRCv2 dataset
-%% and call a function 'extractRandom' to extract a descriptor from the
-%% image.  Currently that function returns just a random vector so should
-%% be changed as part of the coursework exercise.
-%%
-%% (c) John Collomosse 2010  (J.Collomosse@surrey.ac.uk)
-%% Centre for Vision Speech and Signal Processing (CVSSP)
-%% University of Surrey, United Kingdom
-
 close all;
 clear all;
 
-%% Edit the following line to the folder you unzipped the MSRCv2 dataset to
+
 DATASET_FOLDER = '/MATLAB Drive/CW/msrc_objcategimagedatabase_v2/MSRC_ObjCategImageDatabase_v2';
 
-%% Create a folder to hold the results (descriptors extracted by the program)
+
 OUT_FOLDER = '/MATLAB Drive/CW/descriptors';
-%% and within that folder, create another folder to hold these descriptors
-%% the idea is all your descriptors are in individual folders - within
-%% the folder specified as 'OUT_FOLDER'.
-%OUT_SUBFOLDER ='globalRGBhisto';
+
 OUT_SUBFOLDER_AVG_RGB = 'avgRGB';
+DESCRIPTOR_SUBFOLDER_GLOBAL='globalRGBhisto';
+DESCRIPTOR_SUBFOLDER_SPATIAL_COLOUR_GRID = 'SpatialColourGrid';
+DESCRIPTOR_SUBFOLDER_SPATIAL_TEXTURE_GRID = 'SpatialGridTexture'; 
+DESCRIPTOR_SUBFOLDER_EOH = 'EdgeOrientationHisto';
+DESCRIPTOR_SUBFOLDER_MERGE_CEOH = 'ceoh';   %Colour+Edges
 
 
 allfiles=dir (fullfile([DATASET_FOLDER,'/Images/*.bmp']));
+
+% For quantization 4 works better than 3.
+QuantzLevel = 4; %3,4,5,6,7 
+
+grids = 4; %4,6,8,10
+spatialAngularLevel = 4;
+spatialThreshold = 0.29;
+
 for filenum=1:length(allfiles)
     fname=allfiles(filenum).name;
     fprintf('Processing file %d/%d - %s\n',filenum,length(allfiles),fname);
     tic;
     imgfname_full=([DATASET_FOLDER,'/Images/',fname]);
-    img=double(imread(imgfname_full))./255;
-    fout=[OUT_FOLDER,'/',OUT_SUBFOLDER_AVG_RGB,'/',fname(1:end-4),'.mat'];%replace .bmp with .mat
-    F=extractAvgRGB(img);
-   
-    %F=GCH_generator(img,QuantzLevel);
+    img=double(imread(imgfname_full))./255; %% comment for Global RGB.
+    
+    %fout=[OUT_FOLDER,'/',OUT_SUBFOLDER_AVG_RGB,'/',fname(1:end-4),'.mat'];
+    % Extract descriptor from img & returns a random vector
+    % F=extractAvgRGB(img); 
+    
+    %% UNCOMMENT
+    % Call Global Colour histogram to be computed      
+    %fout=[OUT_FOLDER,'/',DESCRIPTOR_SUBFOLDER_GLOBAL,'/',fname(1:end-4),'.mat'];
+    %F = GCH_generator(img, QuantzLevel);
+
+    % Grid Colour histogram
+    fout=[OUT_FOLDER,'/',DESCRIPTOR_SUBFOLDER_SPATIAL_COLOUR_GRID,'/',fname(1:end-4),'.mat'];
+    F = gridColourHistoConstructor(img, grids);
+    
+    % Spatial Grid Texture
+    %fout=[OUT_FOLDER,'/',DESCRIPTOR_SUBFOLDER_SPATIAL_TEXTURE_GRID,'/',fname(1:end-4),'.mat'];
+    %F = gridColourHistoConstructor(img, grids, spatialAngularLevel, spatialThreshold);
+    
+    % CEOH
+    %fout=[OUT_FOLDER,'/',DESCRIPTOR_SUBFOLDER_MERGE_CEOH,'/',fname(1:end-4),'.mat'];
+    %F = gridColourHistoConstructor(img, grids, spatialAngularLevel, spatialThreshold);
+
     save(fout,'F');
     toc
 end
 
-% Here 
-%QuantzLevel = 8;
-%GCH_generator(img,QuantzLevel);
 
-
-
-
+%%                   SOS
+% https://en.wikipedia.org/wiki/Content-based_image_retrieval
 
